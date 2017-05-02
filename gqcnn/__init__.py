6c817c6cbeb9cabd5c1edef1bcdd08f2f9f9870a
Vishal Satish <vsatish@berkeley.edu>
2017-05-02 16:55:08 -0700

    Added __init__.py

---
+++ gqcnn/__init__.py
@@ -0,0 +1,13 @@
+from gqcnn import GQCNN
+from deep_optimizer import DeepOptimizer
+from gqcnn_analyzer import GQCNNAnalyzer
+from optimizer_constants import ImageMode, TrainingMode, PreprocMode, InputDataMode, GeneralConstants, ImageFileTemplates
+from train_stats_logger import TrainStatsLogger
+from learning_analysis import ClassificationResult, RegressionResult
+
+__all__ = ['GQCNN', 
+		   'DeepOptimizer',
+		   'GQCNNAnalyzer',
+		   'ImageMode', 'TrainingMode', 'PreprocMode', 'InputDataMode',
+		   'TrainStatsLogger',
+		   'ClassificationResult', 'RegressionResult']
