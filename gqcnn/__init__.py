e33ba449ad1b53187cbbedc13d7b6b9fbd419f55
visatish <vishalstw@gmail.com>
2017-05-07 00:34:43 -0700

    Added ConfusionMatrix

--- gqcnn/__init__.py
+++ gqcnn/__init__.py
@@ -3,11 +3,11 @@ from deep_optimizer import DeepOptimizer
 from gqcnn_analyzer import GQCNNAnalyzer
 from optimizer_constants import ImageMode, TrainingMode, PreprocMode, InputDataMode, GeneralConstants, ImageFileTemplates
 from train_stats_logger import TrainStatsLogger
-from learning_analysis import ClassificationResult, RegressionResult
+from learning_analysis import ClassificationResult, RegressionResult, ConfusionMatrix
 
 __all__ = ['GQCNN', 
 		   'DeepOptimizer',
 		   'GQCNNAnalyzer',
 		   'ImageMode', 'TrainingMode', 'PreprocMode', 'InputDataMode',
 		   'TrainStatsLogger',
-		   'ClassificationResult', 'RegressionResult']
+		   'ClassificationResult', 'RegressionResult', 'ConfusionMatrix']
