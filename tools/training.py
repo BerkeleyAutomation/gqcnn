48457af187cf42aedfdb62186848f02ea8aeb645
Vishal Satish <vsatish@berkeley.edu>
2017-05-16 23:14:01 -0700

    Fixed bug where code after optimization was not executed. Added timing code for all use-cases.

--- tools/training.py
+++ tools/training.py
@@ -4,6 +4,7 @@ Author: Vishal Satish
 """
 from gqcnn import GQCNN, DeepOptimizer, GQCNNAnalyzer
 from core import YamlConfig
+import time
 
 # train_config = YamlConfig('cfg/tools/train_grasp_quality_cnn_dexnet_large.yaml')
 train_config = YamlConfig('cfg/tools/train_grasp_quality_cnn.yaml')
@@ -11,25 +12,42 @@ gqcnn_config = train_config['gqcnn_config']
 analysis_config = YamlConfig('cfg/tools/analyze_gqcnn_performance.yaml')
 model_dir = '/home/autolab/Public/data/dex-net/data/models/grasp_quality/gqcnn_vgg_mini_dexnet_robust_eps_replication_01_23_17'
 
+def get_elapsed_time(time_in_seconds):
+	""" Helper function to get elapsed time """
+	if time_in_seconds < 60:
+		return '%.1f seconds' % (time_in_seconds)
+	elif time_in_seconds < 360:
+		return '%.1f minutes' % (time_in_seconds / 60)
+	else:
+		return '%.1f hours' % (time_in_seconds / 360)
+
 ###Possible Use-Cases###
 
 # Use Case 1-Prediction
+# start_time = time.time()
 # gqcnn = GQCNN.load(model_dir)
 # gqcnn.predict(images, poses)
 # gqcnn.close_session()
+# print 'Total Prediction Time:', get_elapsed_time(time.time() - start_time)
 
 # Use Case 2-Training from Scratch
-# gqcnn = GQCNN(gqcnn_config)
-# deepOptimizer = DeepOptimizer(gqcnn, train_config)
-# with gqcnn.get_tf_graph().as_default():
-#     deepOptimizer.optimize()
+start_time = time.time()
+gqcnn = GQCNN(gqcnn_config)
+deepOptimizer = DeepOptimizer(gqcnn, train_config)
+with gqcnn.get_tf_graph().as_default():
+    deepOptimizer.optimize()
+print 'Total Training Time:', get_elapsed_time(time.time() - start_time) 
 
 # Use Case 3-Fine Tuning
+# start_time = time.time()
 # gqcnn = GQCNN.load(model_dir)
 # deepOptimizer = DeepOptimizer(gqcnn, train_config)
 # with gqcnn._graph.as_default():
     # deepOptimizer.optimize()
+# print 'Total Fine Tuning Time:', get_elapsed_time(time.time() - start_time)
 
 # Analysis
+start_time = time.time()
 analyzer = GQCNNAnalyzer(analysis_config)
 analyzer.analyze()
+print 'Total Analysis Time:', get_elapsed_time(time.time() - start_time)
