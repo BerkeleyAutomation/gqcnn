773d6d63cce6a4f1e9de52d4af6fce2930c10cba
Vishal Satish <vsatish@berkeley.edu>
2017-06-27 11:06:44 -0700

    Fixed bug in checking whether or not all required training files have been find

--- gqcnn/sgd_optimizer.py
+++ gqcnn/sgd_optimizer.py
@@ -855,7 +855,7 @@ class SGDOptimizer(object):
         self.stable_pose_filenames.sort(key = lambda x: int(x[-9:-4]))
 
         # check valid filenames
-        if len(self.im_filenames) == 0 or len(self.label_filenames) == 0 or len(self.label_filenames) == 0 or len(self.stable_pose_filenames) == 0:
+        if len(self.im_filenames) == 0 or len(self.pose_filenames) == 0 or len(self.label_filenames) == 0 or len(self.stable_pose_filenames) == 0:
             raise ValueError('One or more required training files in the dataset could not be found.')
         if len(self.obj_id_filenames) == 0:
             self.obj_id_filenames = None
