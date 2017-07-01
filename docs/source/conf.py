57de83ffbd3c58b9804c0fd341da782dbfcf4987
jeff <jmahler@berkeley.edu>
2017-06-30 17:35:01 -0700

    Updated sys path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,10 +19,9 @@ import sphinx_rtd_theme
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../'))
-sys.path.insert(0, os.path.abspath('../../examples'))
-sys.path.insert(0, os.path.abspath('../../tools'))
-print os.path.abspath(os.path.dirname(__name__))
+sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__name__)), '../../'))
+sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__name__)), '../../examples'))
+sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__name__)), '../../tools'))
 
 # -- General configuration ------------------------------------------------
 
