46396400aa151bacf78c7acc1fecf00700f3a830
Vishal Satish <vsatish@berkeley.edu>
2018-01-22 20:58:26 -0800

    Modified plot_training_losses.py to only plot neon validation error

--- tools/plot_training_losses.py
+++ tools/plot_training_losses.py
@@ -11,69 +11,29 @@ model_dir : str
     Command line argument, the path to the model whose errors are to plotted. All plots and other metrics will
     be saved to this directory. 
 """
-import IPython
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import sys
+import h5py
 
-TRAIN_LOSS_FILENAME = 'train_losses.npy'
-TRAIN_ERRORS_FILENAME = 'train_errors.npy'
-VAL_ERRORS_FILENAME = 'val_errors.npy'
-TRAIN_ITERS_FILENAME = 'train_eval_iters.npy'
-VAL_ITERS_FILENAME = 'val_eval_iters.npy'
-
+DATABASE_FNAME = 'data.h5'
 
 if __name__ == '__main__':
     result_dir = sys.argv[1]
-    train_errors_filename = os.path.join(result_dir, TRAIN_ERRORS_FILENAME)
-    val_errors_filename = os.path.join(result_dir, VAL_ERRORS_FILENAME)
-    train_iters_filename = os.path.join(result_dir, TRAIN_ITERS_FILENAME)
-    val_iters_filename = os.path.join(result_dir, VAL_ITERS_FILENAME)
+    with h5py.File(os.path.join(result_dir, DATABASE_FNAME), 'r') as f:
+        val_errors = f['metrics']['Error Rate'][:] * 100
+    num_epochs = val_errors.shape[0]
 
-    train_errors = np.load(train_errors_filename)
-    val_errors = np.load(val_errors_filename)
-    train_iters = np.load(train_iters_filename)
-    val_iters = np.load(val_iters_filename)
+    val_iters = np.arange(num_epochs)
 
-    init_val_error = val_errors[0]
-    norm_train_errors = train_errors / init_val_error
-    norm_val_errors = val_errors / init_val_error
+    print ('Final val error: {}'.format(val_errors[-1]))
 
-    print 'Final val error', val_errors[-1]
-
-    plt.figure()
-    plt.plot(train_iters, train_errors, linewidth=4, color='b')
+    plt.figure(figsize=(8, 6))
+    plt.margins(x=0.05)
     plt.plot(val_iters, val_errors, linewidth=4, color='g')
-    plt.ylim(0, 100)
-    plt.legend(('Training (Minibatch)', 'Validation'), fontsize=15, loc='best')
-    plt.xlabel('Iteration', fontsize=15)
+    plt.ylim(0, 40)
+    plt.legend(('Validation',), fontsize=15, loc='upper right')
+    plt.xlabel('Epoch', fontsize=15)
     plt.ylabel('Error Rate', fontsize=15)
- 
-    plt.figure()
-    plt.plot(train_iters, norm_train_errors, linewidth=4, color='b')
-    plt.plot(val_iters, norm_val_errors, linewidth=4, color='g')
-    plt.ylim(0, 2.0)
-    plt.legend(('Training (Minibatch)', 'Validation'), fontsize=15, loc='best')
-    plt.xlabel('Iteration', fontsize=15)
-    plt.ylabel('Normalized Error Rate', fontsize=15)
-    plt.show()
- 
-    plt.figure(figsize=(8,6))
-    plt.plot(train_iters, train_errors, linewidth=4, color='b')
-    plt.plot(val_iters, val_errors, linewidth=4, color='g')
-    plt.ylim(0, 100)
-    plt.legend(('Training (Minibatch)', 'Validation'), fontsize=15, loc='best')
-    plt.xlabel('Iteration', fontsize=15)
-    plt.ylabel('Error Rate', fontsize=15)
-    plt.savefig(os.path.join(result_dir, 'training_curve.jpg'))
- 
-    plt.figure(figsize=(8,6))
-    plt.plot(train_iters, norm_train_errors, linewidth=4, color='b')
-    plt.plot(val_iters, norm_val_errors, linewidth=4, color='g')
-    plt.ylim(0, 2.0)
-    plt.legend(('Training (Minibatch)', 'Validation'), fontsize=15, loc='best')
-    plt.xlabel('Iteration', fontsize=15)
-    plt.ylabel('Normalized Error Rate', fontsize=15)
-    plt.savefig(os.path.join(result_dir, 'normalized_training_curve.jpg'))
-  
+    plt.savefig(os.path.join(result_dir, 'validation_error.png')) 
