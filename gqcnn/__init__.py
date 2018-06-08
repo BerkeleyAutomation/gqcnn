da6f84bb77239c60a75fabeef2a50eead4866157
Vishal Satish <vsatish@berkeley.edu>
2018-06-07 17:16:12 -0700

    Updated policies

--- gqcnn/__init__.py
+++ gqcnn/__init__.py
@@ -3,13 +3,13 @@ from visualizer import Visualizer
 from image_grasp_sampler import ImageGraspSampler, AntipodalDepthImageGraspSampler, ImageGraspSamplerFactory
 from grasp_quality_function import GraspQualityFunctionFactory
 from policy import RgbdImageState, ParallelJawGrasp
-from policy import Policy, GraspingPolicy, AntipodalGraspingPolicy, CrossEntropyAntipodalGraspingPolicy, QFunctionAntipodalGraspingPolicy, EpsilonGreedyQFunctionAntipodalGraspingPolicy, FullyConvolutionalAngularPolicyTopK, FullyConvolutionalAngularPolicyImportance, UniformRandomGraspingPolicy
+from policy import Policy, GraspingPolicy, AntipodalGraspingPolicy, CrossEntropyAntipodalGraspingPolicy, QFunctionAntipodalGraspingPolicy, EpsilonGreedyQFunctionAntipodalGraspingPolicy, FullyConvolutionalAngularPolicyTopK, FullyConvolutionalAngularPolicyImportance, FullyConvolutionalAngularPolicyUniform, UniformRandomGraspingPolicy, CrossEntropyRobustGraspingPolicy
 from gqcnn_prediction_visualizer import GQCNNPredictionVisualizer
 from gqcnn_analyzer import GQCNNAnalyzer
 
 __all__ = ['Grasp2D', 'SuctionPoint2D',
            'ImageGraspSampler', 'AntipodalDepthImageGraspSampler', 'ImageGraspSamplerFactory'
            'Visualizer', 'RobotGripper',
-           'ParallelJawGrasp', 'Policy', 'GraspingPolicy', 'AntipodalGraspingPolicy', 'CrossEntropyAntipodalGraspingPolicy', 'FullyConvolutionalAngularPolicyTopK', 'FullyConvolutionalAngularPolicyImportance', 'UniformRandomGraspingPolicy',
+           'ParallelJawGrasp', 'Policy', 'GraspingPolicy', 'AntipodalGraspingPolicy', 'CrossEntropyAntipodalGraspingPolicy', 'FullyConvolutionalAngularPolicyTopK', 'FullyConvolutionalAngularPolicyImportance', 'FullyConvolutionalAngularPolicyUniform', 'UniformRandomGraspingPolicy', 'CrossEntropyRobustGraspingPolicy',
            'RgbdImageState',
            'GQCNNPredictionVisualizer']
