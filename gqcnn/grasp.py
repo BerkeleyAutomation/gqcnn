3f79c1f47bd9760e78a6d2325222bebdc7c149cb
jeff <jmahler@berkeley.edu>
2018-10-11 13:56:54 -0700

    Fixed a bug in the suction approach axis normal correction

--- gqcnn/grasp.py
+++ gqcnn/grasp.py
@@ -366,7 +366,7 @@ class SuctionPoint2D(object):
         suction_x_camera = suction_axis_camera
         suction_z_camera = np.array([-suction_x_camera[1], suction_x_camera[0], 0])
         if np.linalg.norm(suction_z_camera) < 1e-12:
-            suction_z_camera = np.array([0.0, 0.0, 1.0])
+            suction_z_camera = np.array([1.0, 0.0, 0.0])
         suction_z_camera = suction_z_camera / np.linalg.norm(suction_z_camera)
         suction_y_camera = np.cross(suction_z_camera, suction_x_camera)
         suction_rot_camera = np.c_[suction_x_camera, suction_y_camera, suction_z_camera]
