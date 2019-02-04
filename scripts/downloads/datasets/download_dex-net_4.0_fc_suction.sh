ce7cfc1be44e12ff0afdae4b545f80c05af186ee
Vishal Satish <vsatish@berkeley.edu>
2019-02-04 05:55:37 -0800

    Adding download scripts.

---
+++ scripts/downloads/datasets/download_dex-net_4.0_fc_suction.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+wget -O data/training/dexnet_3.tar.gz https://berkeley.box.com/shared/static/wd5s51f1n786i71t8dufckec0262za4f.gz
+
+cd data/training
+tar -xvzf dexnet_3.tar.gz
+mv dexnet_3 dex-net_3.0
+cd ../..
