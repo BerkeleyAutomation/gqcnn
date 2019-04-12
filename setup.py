2b06ed2398460d46b4f8cd4140c1a869ac2ead8b
Vishal Satish <vsatish@berkeley.edu>
2019-04-12 04:09:41 -0700

    WIP modifying setup.py for docker.

--- setup.py
+++ setup.py
@@ -26,27 +26,97 @@ Author
 ------
 Jeff Mahler & Vishal Satish
 """
-import os
+import sys
 import logging
+import subprocess
 from setuptools import setup, find_packages
 from setuptools.command.develop import develop
 from setuptools.command.install import install
-from subprocess import check_output
+
+TF_MIN_VERSION = '1.10.0'
+TF_MAX_VERSION = '1.13.1'
+DOWNLOAD_EXAMPLE_DATA_SCRIPT = 'scripts/downloads/download_example_data.sh'
 
 # set up logger
 logging.basicConfig() # configure the root logger
 logger = logging.getLogger('setup.py')
 logger.setLevel(logging.INFO)
 
-class PostDevelopCmd(develop):
-    def run(self):
-        develop.run(self)
-        os.system('sh scripts/downloads/download_example_data.sh')
+def get_tf_dep():
+    # check whether or not the Nvidia driver and GPUs are available and add the corresponding Tensorflow dependency
+    tf_dep = 'tensorflow>={},<={}'.format(TF_MIN_VERSION, TF_MAX_VERSION)
+    try:
+        gpus = subprocess.check_output(['nvidia-smi', '--query-gpu=gpu_name', '--format=csv']).decode().strip().split('\n')[1:]
+        if len(gpus) > 0:
+            tf_dep = 'tensorflow-gpu>={},<={}'.format(TF_MIN_VERSION, TF_MAX_VERSION)
+        else:
+            logger.warning('Found Nvidia device driver but no devices...installing Tensorflow for CPU.')
+    except OSError:
+        logger.warning('Could not find Nvidia device driver...installing Tensorflow for CPU.')
+    return tf_dep
 
-class PostInstallCmd(install):
+def download_example_data():
+    subprocess.Popen(['sh', DOWNLOAD_EXAMPLE_DATA_SCRIPT]).wait()
+
+class DevelopCmd(develop):
+    user_options_custom = [
+        ('docker', None, 'this is a docker installation'),
+    ]
+    user_options = getattr(develop, 'user_options', []) + user_options_custom
+
+    def initialize_options(self):
+        develop.initialize_options(self)
+
+        # initialize options
+        self.docker = False
+
+    def finalize_options(self):
+        develop.finalize_options(self)
+
     def run(self):
+        # install Tensorflow dependency
+        if not self.docker:
+            tf_dep = get_tf_dep()
+            subprocess.Popen([sys.executable, '-m', 'pip', 'install', tf_dep]).wait()
+        else:
+            # if we're using docker, this will already have been installed explicitly through the correct requirements.txt; there is no way to check for CUDA/GPUs at docker build time because there is no easy way to set the nvidia runtime
+            logger.warning('Omitting Tensorflow dependency because of Docker installation.') #TODO(vsatish): Figure out why this isn't printed
+
+        # run installation
+        develop.run(self)
+
+        # download example data
+        download_example_data()
+
+class InstallCmd(install, object):
+    user_options_custom = [
+        ('docker', None, 'this is a docker installation'),
+    ]
+    user_options = getattr(install, 'user_options', []) + user_options_custom
+
+    def initialize_options(self):
+        install.initialize_options(self)
+
+        # initialize options
+        self.docker = False
+
+    def finalize_options(self):
+        install.finalize_options(self)
+
+    def run(self):
+        # install Tensorflow dependency
+        if not self.docker:
+            tf_dep = get_tf_dep()
+            subprocess.Popen([sys.executable, '-m', 'pip', 'install', tf_dep]).wait()
+        else:
+            # if we're using docker, this will already have been installed explicitly through the correct requirements.txt; there is no way to check for CUDA/GPUs at docker build time because there is no easy way to set the nvidia runtime
+            logger.warning('Omitting Tensorflow dependency because of Docker installation.') #TODO(vsatish): Figure out why this isn't printed
+
+        # run installation
         install.run(self)
-        os.system('sh scripts/downloads/download_example_data.sh')
+
+        # download example data
+        download_example_data()
 
 requirements = [
     'autolab-core',
@@ -61,19 +131,6 @@ requirements = [
     'psutil',
     'gputil'
 ]
-
-# check whether or not the Nvidia driver and GPUs are available and add the corresponding Tensorflow dependency
-tf_min_version = '1.10.0'
-tf_max_version = '1.13.1'
-try:
-    gpus = check_output(['nvidia-smi', '--query-gpu=gpu_name', '--format=csv']).decode().strip().split('\n')[1:]
-    if len(gpus) > 0:
-        tf_dep = 'tensorflow-gpu>={},<={}'.format(tf_min_version, tf_max_version)
-    else:
-        logger.warning('Found Nvidia device driver but no devices...installing Tensorflow for CPU.')
-except OSError:
-    logger.warning('Could not find Nvidia device driver...installing Tensorflow for CPU.')
-requirements.append(tf_dep)
 
 exec(open('gqcnn/version.py').read())
 
@@ -100,7 +157,8 @@ setup(name='gqcnn',
       ],
       },
       cmdclass={
-        'install': PostInstallCmd,
-        'develop': PostDevelopCmd
+        'install': InstallCmd,
+        'develop': DevelopCmd
       }
 )
+
