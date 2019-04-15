19779ea15ab8629b6990ee27446cb69ade9aca10
Vishal Satish <vsatish@berkeley.edu>
2019-04-15 13:55:08 -0700

    Updated branch to tar for docker installation to master.

--- scripts/docker/build-docker.sh
+++ scripts/docker/build-docker.sh
@@ -1,6 +1,6 @@
 # build the CPU and GPU docker images
 
-git archive --format=tar -o docker/gqcnn.tar --prefix=gqcnn/ docker
+git archive --format=tar -o docker/gqcnn.tar --prefix=gqcnn/ master
 docker build --no-cache -t gqcnn/gpu -f docker/gpu/Dockerfile .
 docker build --no-cache -t gqcnn/cpu -f docker/cpu/Dockerfile .
 rm docker/gqcnn.tar
