bb82b67710202e33dce63013d28cf1892daa8967
Vishal Satish <vsatish@berkeley.edu>
2019-05-29 13:25:06 -0700

    Made meshrender dep conditional on Python 2.

--- setup.py
+++ setup.py
@@ -141,10 +141,15 @@ class InstallCmd(install, object):
 
 
 requirements = [
-    "autolab-core", "autolab-perception", "meshrender==0.0.9", "visualization", "numpy", "scipy",
+    "autolab-core", "autolab-perception", "visualization", "numpy", "scipy",
     "matplotlib<=2.2.0", "opencv-python", "scikit-image<=0.14.2", "scikit-learn", "psutil",
     "gputil"
 ]
+
+if sys.version[0] == "2":
+    # The Python 2 meshrender installation is broken, and we don't know
+    # when it will be fixed.
+    requirements.append("meshrender==0.0.9")
 
 exec(
     open(
