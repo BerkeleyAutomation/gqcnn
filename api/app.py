fa9798fc05035beb8eea980574ca4127649b4d8f
nwjbrandon <nwjbrandon@gmail.com>
2020-06-12 22:15:25 +0800

    add web services

---
+++ api/app.py
@@ -0,0 +1,12 @@
+import logging
+
+from flask import Flask
+from flask_cors import CORS
+
+from api.blueprint import app_blueprint
+
+app = Flask(__name__)
+
+app.register_blueprint(app_blueprint)
+
+CORS(app)
---
+++ api/blueprint.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, jsonify
+
+from api.service import heathcheck_service, grasp_planning_service
+
+app_blueprint = Blueprint("app_blueprint", __name__)
+
+
+@app_blueprint.route("/", methods=["GET"])
+def heathcheck_service_api():
+    return jsonify(heathcheck_service())
+
+
+@app_blueprint.route("/grasp-planning", methods=["GET"])
+def grasp_planning_api():
+    return jsonify(grasp_planning_service())
